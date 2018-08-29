3551b65dc8318694a5539518abac514c2433160b
devcontract <38960225+devcontract@users.noreply.github.com>
2018-08-29 12:05:22 +0300

    8

--- 1-work/index.php
+++ 1-work/index.php
@@ -9,14 +9,18 @@ $eth = new Ethereum('127.0.0.1', 8545);
 
 $addr = "0x4E81103607839B3895AEEeb2EA5899a8De1C1F5d";
 
-$accounts = $eth->eth_accounts();
-$encoded_accounts =serialize($accounts); //json_encode($accounts);
-echo count($accounts);
-echo $accounts[2];
 
-           //$eth-> genPair()
-          //   echo $account , PHP_EOL;
-          // }
+   $count = $ethFun->countAccounts();
+   $allaccounts = $eth->eth_accounts();
+   $array;
+     for ($i=0; $i < $count; $i++) {
+       $address = $allaccounts[$i];
+       $balance = $ethFun->getBalanceOfAddress($address);
+       $array[$i] = $balance["balance"];
+     }
+     $encArray = json_encode($array);
+     echo   $encArray;
+
 
 
 ?>
