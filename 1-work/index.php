911d6c250424c68e0fe66bc41c917883041ba45f
devcontract <38960225+devcontract@users.noreply.github.com>
2018-08-29 12:04:54 +0300

    7

--- 1-work/index.php
+++ 1-work/index.php
@@ -1,12 +1,22 @@
 <?php
-   include ("eth.php");
-   $addr = "0x4E81103607839B3895AEEeb2EA5899a8De1C1F5d";
-    $data =  $ethFun -> getBalanceOfAddress($addr);
-       echo $data['balance'];
-       //echo $data['pending'];     //Prints John
+
+include ("eth.php");
+//require 'libs/ethereum-php/ethereum.php';
+// create a new object
+$eth = new Ethereum('127.0.0.1', 8545);
+// do your thing
 
 
+$addr = "0x4E81103607839B3895AEEeb2EA5899a8De1C1F5d";
 
-      $rr =  $ethFun->genPair();
-      echo $rr;
+$accounts = $eth->eth_accounts();
+$encoded_accounts =serialize($accounts); //json_encode($accounts);
+echo count($accounts);
+echo $accounts[2];
+
+           //$eth-> genPair()
+          //   echo $account , PHP_EOL;
+          // }
+
+
 ?>
